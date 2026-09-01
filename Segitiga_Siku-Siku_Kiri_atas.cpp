02c14fd074473ef527223614c6f97121cdc7830d
fedrodzakwan <fedroddelmora@gmail.com>
2026-09-01 21:19:06 +0700

    menambahkan latihan 2 segitiga siku siku kiri atas

---
+++ Segitiga_Siku-Siku_Kiri_atas.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "masukan jumlah n";
+    cin >> n;
+
+    for (int i=1 ;i<=n;i++){
+        for (int j=5; j>=i ;j--){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+
+
+    
+
+    return 0;
+}
