440bfa5338413e6c1ee7a52d922bf7616442cec4
fedrodzakwan <fedroddelmora@gmail.com>
2026-09-03 14:34:43 +0700

    Masukan nilai n

---
+++ 010.Pola_Angka_Selang-Seling.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "masukan nilai n";
+    cin >> n;
+
+    
+    for (int i=1;i<=n-1;i++){
+        for (int j=1;j<=i;j++){
+            if((i%2==0 && j%2==0 || i%2==1 && j%2==1)){
+                cout << 1;
+            }
+            else{
+                cout << 0;
+            }
+            
+        }
+    cout << endl;
+}
+    
+    return 0;
+}
