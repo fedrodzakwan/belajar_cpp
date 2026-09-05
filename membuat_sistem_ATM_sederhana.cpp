85a70a058b1efde890f2339bfa3bcd71cfae0f5e
fedrodzakwan <fedroddelmora@gmail.com>
2026-09-05 13:50:21 +0700

    menambahkan latihan sistem atm sederhana

--- membuat_sistem_ATM_sederhana.cpp
+++ membuat_sistem_ATM_sederhana.cpp
@@ -10,35 +10,88 @@ bool cekpin(int pin){
     }
 }
 
+
+
+int proses_penarikan(int saldo,int penarikan){
+    if (penarikan <= saldo)
+    {
+        int sisasaldo = saldo-penarikan;
+        cout << "sisa saldo anda" << sisasaldo;
+        return sisasaldo;
+    }
+    else{
+        cout << "saldo anda tidak cukup bang";
+        return saldo;
+    }
+}
+
+
 int main() {
 
-    bool cekpasword=false;
+    bool cekstatus=false;
+    int cekpasword;
+    bool cekstatus2;
+    int saldo = 1000000;
+    int penarikan;
+    int sisa_saldo;
+    bool cekpenarikan=true;
+    bool cekpenarikan1 = true;
+    string konfirmasi;
 
-    while (cekpasword==false){
-        cout << "Masukan pasword";
+    while (cekstatus==false){
+        cout << "masukan pasword";
         cin >> cekpasword;
 
-        bool cek = cekpin(cekpasword);
+        cekstatus2 = cekpin(cekpasword);
 
-        if (cek == false){
-            "pasword anda salah";   
+       
+        if (cekpasword < 1000 or cekpasword > 9999){
+            cout << "harap masukan 4 digit";
+            cout << endl;
+            cekstatus = false;
         }
-        else if (cekpasword < 1000 or cekpasword > 9999)
-        {
-            cout << "silahkan masukan 4 digit";
+        else if (cekstatus2 == false){
+            cout << "pasword yang anda masukan salah silahkan coba lagi";
+            cout << endl;
+            cekstatus = false;
+        }
+        else{
+            cekstatus = true;
         }
     }
 
-
     cout << "anda berhasil masuk";
 
-    
-    
+    cout << "silahkan mau tarik berapa" << endl;
+    cout << 100000 << endl;
+    cout << 50000 << endl;
 
-  
+    while (cekpenarikan == true)
+    {
+        cout << "masukan pilihan";
+        cin >> penarikan;
 
-    
+        saldo = proses_penarikan(saldo,penarikan);
+        cekpenarikan1 =true;
 
-    
+        if(cekpenarikan1 == true){
+            cout << "apakah mau menarik lagi?";
+            cin >> konfirmasi;
+            if (konfirmasi == "ya"){
+            cekpenarikan = true;
+            cekpenarikan1 = false;
+            }
+            else if (konfirmasi == "No"){
+            cekpenarikan =false;
+            }
+            else{
+            cout << "harap masukan sesuai perintah";
+            cekpenarikan = false;
+        
+        }
+        
+        }
+
+    }
     return 0;
 }
