b7c2d682c115bdb01b13481f3a9aef34b0e9df13
fedrodzakwan <fedroddelmora@gmail.com>
2026-09-06 18:56:43 +0700

    memperbaiki

--- 16.hitungdiskon.cpp
+++ 16.hitungdiskon.cpp
@@ -2,15 +2,12 @@
 using namespace std;
 
 double hitungdiskon(int totalbelanja,double diskon){
-    double hasil;
     if (totalbelanja >= 100000){
         double hasil_diskon  = totalbelanja * diskon;
-        double hasil =  totalbelanja - hasil_diskon ;
-        cout << " anda mendapatkan diskon sebesar 10% an total belanjaan kamu adalah " << hasil;
-        return hasil;
+        cout << "Mendapatkan diskon " << hasil_diskon << endl;
+        return hasil_diskon;
     }
     else{
-        cout << "total belanjaan anda " << totalbelanja;
         return 0;
     }
 
@@ -25,24 +22,34 @@ int main() {
     int hargabarang;
     int total=0;
     double diskon = 0.10;
+    int total_hargas;
+    double potongan_harga;
 
 
 
     while (status_belanja == true){
 
-        cout << "Masukan total beli" ;
+        cout << "Masukan total beli " ;
         cin >> total_beli;
 
         for (int i=1;i<=total_beli;i++){
             cout << "Masukan harga barang ";
             cin >> hargabarang;
 
+            cout << "barang ke " << "-" << i <<" "<< hargabarang << endl;
             total = total + hargabarang;
-
-            cout << "total belanja " << total;
         }
+        cout << "Total belanjaan: " << total << endl;
 
-        hitungdiskon(total,diskon);
+        potongan_harga = hitungdiskon(total,diskon);
+
+
+
+        total_hargas = total - potongan_harga;
+
+        cout << "total harga barang " << total_hargas;
+
+
 
         status_belanja = false;
             
